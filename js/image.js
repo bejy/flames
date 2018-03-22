3ed2306e44220d80cb310b51fe1591b280749215
bejy <noitshilarious@gmail.com>
2018-03-22 10:52:29 -0700

    first commit

---
+++ js/image.js
@@ -0,0 +1,13 @@
+function image(a,b,c)
+{
+	this.link = a;
+	this.alt = b;
+	this.thumb = c;
+}
+
+function show_image()
+{
+	document.write("img src = "Chrysanthemum.jpg");
+}
+
+image1 = new image("img/Chrysanthemum.jpg","dsssds","thumb/img3");
